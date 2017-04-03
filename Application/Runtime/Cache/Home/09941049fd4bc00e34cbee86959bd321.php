638024de061fa09a864e314ac00a5d6836a4c229
Egist <maysrp@gmail.com>
2017-04-03 17:39:44 +0800

    view change

--- Application/Runtime/Cache/Home/09941049fd4bc00e34cbee86959bd321.php
+++
@@ -1,6 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><form action="" method="post">
-	<input type="text" name="name">
-	<input type="password" name="password">
-	<input type="password" name="password_2">
-	<input type="email" name="email">
-</form>
